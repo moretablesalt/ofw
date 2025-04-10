f0605166c029051e24a6e7eaf398723644ddd3a5
Mark Tiu <mark.tiu@prudentialguarantee.com>
2025-04-10 18:31:08 +0800

    Make navbar mobile responsive

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
