b65c2b7b18732cfafbe3d4e7adcb8189b489e291
Mark Tiu <mark.tiu@prudentialguarantee.com>
2025-04-17 05:07:45 +0800

    Make hero button scroll down.

--- src/app/features/home/hero/hero.component.ts
+++ src/app/features/home/hero/hero.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
-
+  scrollToSection() {
+    const element = document.getElementById('products');
+    if (element) {
+      const yOffset = -60; // adjust based on your navbar height
+      const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  }
 }
