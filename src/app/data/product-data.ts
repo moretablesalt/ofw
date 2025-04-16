2d801810385b32d7ba02ee25e93523a36848c7ee
Mark Tiu <mark.tiu@prudentialguarantee.com>
2025-04-16 17:25:15 +0800

    Disable product 1 and enable product 3

--- src/app/data/product-data.ts
+++ src/app/data/product-data.ts
@@ -50,5 +50,30 @@ export const PRODUCTS: Product[] = [
       { coverage: 'Medical Evacuation', amount: 'Actual Cost' },
       { coverage: 'Medical Repatriation', amount: 'Actual Cost' }
     ]
-  }
+  },
+  {
+    id: 'C',
+    seaCoverages: [
+      { coverage: 'Accidental Death (Loss of Life)', amount: 'USD 55,000.00' },
+      { coverage: 'Natural Death', amount: 'USD 10,000.00', note: 'Underwritten by First Life Financial Company Inc.' },
+      { coverage: 'Permanent Total Disablement', amount: 'USD 7,500.00' },
+      { coverage: 'Repatriation Cost (Death and/or Termination of Employment)', amount: 'Actual Cost' },
+      { coverage: 'Subsistence Allowance', amount: 'USD 100.00 per month, max of (6) months' },
+      { coverage: 'Money claims arising from the Employer\'s liability', amount: 'USD 1,000.00 per month, max of (6) months' },
+      { coverage: 'Compassionate Visit', amount: 'Actual Cost' },
+      { coverage: 'Medical Evacuation', amount: 'Actual Cost' },
+      { coverage: 'Medical Repatriation', amount: 'Actual Cost' }
+    ],
+    landCoverages: [
+      { coverage: 'Accidental Death (Loss of Life)', amount: 'USD 66,000.00' },
+      { coverage: 'Natural Death', amount: 'USD 10,000.00', note: 'Underwritten by First Life Financial Company Inc.' },
+      { coverage: 'Permanent Total Disablement', amount: 'USD 7,500.00' },
+      { coverage: 'Repatriation Cost (Death and/or Termination of Employment)', amount: 'Actual Cost' },
+      { coverage: 'Subsistence Allowance', amount: 'USD 100.00 per month, max of (6) months' },
+      { coverage: 'Money claims arising from the Employer\'s liability', amount: 'USD 1,000.00 per month, max of (6) months' },
+      { coverage: 'Compassionate Visit', amount: 'Actual Cost' },
+      { coverage: 'Medical Evacuation', amount: 'Actual Cost' },
+      { coverage: 'Medical Repatriation', amount: 'Actual Cost' }
+    ]
+  },
 ]
