dd24cffe2ff76bcafc083ddd53aeaa76cdea732a
Mark Tiu <mark.tiu@prudentialguarantee.com>
2025-04-29 03:02:48 +0800

    Initial commit for the payload

--- src/app/features/policy/review/review.component.ts
+++ src/app/features/policy/review/review.component.ts
@@ -2,12 +2,17 @@ import { Component, inject, OnInit } from '@angular/core';
 import { StepsService } from '../../shared/steps/steps.service';
 import { Router } from '@angular/router';
 import { ApplicationFormStorageService } from '../../../services/application-form-storage.service';
-import { DatePipe } from '@angular/common';
+import { DatePipe, JsonPipe } from '@angular/common';
+import { QuoteCalculatorService } from '../../../services/quote-calculator.service';
+import { QuoteDetailsStorageService } from '../../../services/quote-details-storage.service';
+import {ApplicationBuilderService} from '../../../services/application-builder.service';
+import {switchMap} from 'rxjs';
 
 @Component({
   selector: 'app-review',
   imports: [
-    DatePipe
+    DatePipe,
+    JsonPipe
   ],
   templateUrl: './review.component.html',
   styleUrl: './review.component.css'
@@ -17,21 +22,147 @@ export class ReviewComponent implements OnInit {
   private stepsService = inject(StepsService);
   private router = inject(Router);
   private storage = inject(ApplicationFormStorageService);
+  private quoteCalculatorService = inject(QuoteCalculatorService);
+  private quoteDetailsStorageService = inject(QuoteDetailsStorageService);
+  private applicationBuilderService = inject(ApplicationBuilderService);
 
-  formData: any;
+  formData!: any;
+  payload: any;
+  refCode: any;
+  hash: any;
+
+  // pesopay form
+  paymentUrl = 'https://test.pesopay.com/b2cDemo/eng/payment/payForm.jsp';
+  merchantId = '18067292';
+  amount: any;
+  successUrl= 'http://localhost:4200/confirmation';
+  failUrl = 'http://localhost:4200/confirmation';
+  cancelUrl = 'http://localhost:4200/confirmation';
 
   ngOnInit(): void {
     this.stepsService.setStep(3);
     this.formData = this.storage.getAppFormData();
-
+    this.payload = this.mapToPayload()
   }
 
   continue() {
-    this.router.navigate(['/confirmation']);
+    this.applicationBuilderService.postData(this.payload)
+      .pipe(
+        switchMap(response => {
+          const refCode = response.referenceCode;
+          this.refCode = refCode;
+          this.amount = this.quoteCalculatorService.getQuote() !== null ? this.quoteCalculatorService.getQuote()!.toString() : '';
+          return this.applicationBuilderService.generateHash(refCode, this.amount);
+        })
+      ).subscribe(hash => {
+        this.hash = hash.secretHash;
+        if (hash) {
+          this.createAndSubmitPaymentForm();
+        } else {
+          alert('Error generating hash');
+      }
+    });
   }
 
   goBack(): void {
     this.router.navigate(['/policy/application-form']);
   }
 
+  mapToPayload() {
+    return {
+      isCreatedViaAgent: false,
+      startDate: this.quoteDetailsStorageService.getStartDateClean(),
+      endDate: this.quoteDetailsStorageService.getEndDateClean(),
+      product: {
+        code: "OFW_COMPULSORY"
+      },
+      totalNumberOfTravelers: 1,
+      travelers: [
+        {
+          firstName: this.formData.personal.firstName,
+          middleName: this.formData.personal.middleInitial,
+          lastName: this.formData.personal.lastName,
+          sequenceNo: 1,
+          isPrimary: true,
+          dateOfBirth: this.formData.personal.birthDate,
+          email: this.formData.personal.email,
+          age: this.formData.personal.age.toString(),
+          contactNumber: this.formData.personal.mobile,
+          hasTIN: !!this.formData.personal.tin,
+          taxIdentificationNumber: this.formData.personal.tin,
+          streetAddress1: this.formData.personal.address,
+          premium: {
+            totalPremium: this.quoteCalculatorService.getQuote(),
+          },
+          passport: {
+            lastName: this.formData.passport.lastName,
+            firstName: this.formData.passport.firstName,
+            middleName: this.formData.passport.middleName,
+            passportNo: this.formData.passport.passportNo,
+            issuedOn: this.formData.passport.issuedOn,
+            issuedAt: this.formData.passport.issuedAt
+          },
+          work: {
+            companyName: this.formData.work.companyName,
+            address: this.formData.work.address,
+            country: this.formData.work.country,
+            industry: this.formData.work.industry,
+            vesselName: this.formData.work.vesselName,
+            designation: this.formData.work.designation,
+            startDate: this.quoteDetailsStorageService.getStartDateClean(),
+            endDate: this.quoteDetailsStorageService.getEndDateClean(),
+            periodCoverage: this.quoteDetailsStorageService.getPeriod()
+          },
+          totalSumInsured: 198000
+        }
+      ],
+      paymentType: "PAYMENT_GATEWAY",
+      agency: this.formData.agency.agencyName
+        ? {
+          id: 1,
+          name: this.formData.agency.agencyName
+        }
+        : undefined
+    };
+  }
+
+  private createAndSubmitPaymentForm(): void {
+    const form = document.createElement('form');
+    form.method = 'POST';
+    form.action = this.paymentUrl; // Payment Gateway URL
+    form.style.display = 'none'; // Hide form from UI
+
+    // 🔹 Create and append hidden input fields
+    Object.entries(this.getPaymentFormData()).forEach(([key, value]) => {
+      const input = document.createElement('input');
+      input.type = 'hidden';
+      input.name = key;
+      input.value = value;
+      form.appendChild(input);
+    });
+
+    document.body.appendChild(form);
+    form.submit();
+  }
+
+  private getPaymentFormData(): { [key: string]: string } {
+    return {
+      merchantId: this.merchantId,
+      amount: this.amount,
+      orderRef: this.refCode,
+      currCode: '608',
+      successUrl: this.successUrl,
+      failUrl: this.failUrl,
+      cancelUrl: this.cancelUrl,
+      payType: 'N',
+      lang: 'E',
+      mpsMode: 'NIL',
+      payMethod: 'ALL',
+      secureHash: this.hash,
+      remark: '',
+      redirect: '',
+      oriCountry: '',
+      destCountry: '',
+    };
+  }
 }
--- src/app/services/quote-details-storage.service.ts
+++ src/app/services/quote-details-storage.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { TimeHelperService } from './time-helper.service';
+import { format } from 'date-fns';
 
 export class QuoteModel {
   insuranceType!: string;
@@ -13,6 +15,7 @@ export class QuoteModel {
 export class QuoteDetailsStorageService {
   private readonly storageKey = 'quote-details';
   private quoteDetails$ = new BehaviorSubject<QuoteModel | null>(null);
+  private timeHelerService = inject(TimeHelperService);
 
   constructor() {
     const saved = sessionStorage.getItem(this.storageKey);
@@ -37,5 +40,29 @@ export class QuoteDetailsStorageService {
   clear(): void {
     this.quoteDetails$.next(null);
     sessionStorage.removeItem(this.storageKey);
+  }
+
+  getStartDateClean() {
+    const startDate = this.getQuoteDetails()?.startDate;
+    if (startDate) {
+      return format(new Date(startDate), 'yyyy-MM-dd');
+    }
+    return null;
+  }
+
+  getEndDateClean() {
+    const endDate = this.getQuoteDetails()?.endDate;
+    if (endDate) {
+      return format(new Date(endDate), 'yyyy-MM-dd');
+    }
+    return null;
+  }
+
+  getPeriod() {
+    const details = this.getQuoteDetails();
+    if (details?.startDate && details?.endDate) {
+      return this.timeHelerService.calculateMonths(details.startDate, details.endDate);
+    }
+    return 0;
   }
 }
